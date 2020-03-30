901f727d3789e990a106286817e09aa30411b014
AshishB29 <ashish.bhatia2909@gmail.com>
2020-03-31 01:30:27 +0530

    First

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/movie-ang'));
+app.use(express.static(__dirname + 'dist/Angular-movies'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/movie-ang/index.html'));
+res.sendFile(path.join(__dirname+'/dist/Angular-movies/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
